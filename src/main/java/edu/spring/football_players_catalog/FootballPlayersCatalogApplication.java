d6829d0ab5e8fcf69e6d1addebebe66ba7576c09
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-06-25 16:30:04 +0500

    init git repository

---
+++ src/main/java/edu/spring/football_players_catalog/FootballPlayersCatalogApplication.java
@@ -0,0 +1,13 @@
+package edu.spring.football_players_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPlayersCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballPlayersCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/spring/football_players_catalog/FootballPlayersCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.football_players_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballPlayersCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
